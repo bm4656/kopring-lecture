b535b364addf77bee9e1107f5bd2d5bf389c6c01
bm4656 <newjin46@gmail.com>
2025-03-06 02:28:25 +0900

    feat: 엔티티 개발 - 연관관계 있음

--- src/main/kotlin/com/bomin/portfolio/domain/entity/Project.kt
+++ src/main/kotlin/com/bomin/portfolio/domain/entity/Project.kt
@@ -3,10 +3,74 @@ package com.bomin.portfolio.domain.entity
 import jakarta.persistence.*
 
 @Entity
-class Project : BaseEntity() {
+class Project(
+    name: String,
+    description: String,
+    startYear: Int,
+    startMonth: Int,
+    endYear: Int?,
+    endMonth: Int?,
+    isActive: Boolean
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "project_id")
     var id: Long? = null
+
+    var name: String = name
+
+    var description: String = description
+
+    var startYear: Int = startYear
+
+    var startMonth: Int = startMonth
+
+    var endYear: Int? = endYear
+
+    var endMonth: Int? = endMonth
+
+    var isActive: Boolean = isActive
+
+    @OneToMany(
+        targetEntity = ProjectDetail::class,
+        fetch = FetchType.LAZY,
+        cascade = [CascadeType.ALL]
+    )
+    @JoinColumn(name = "project_id")
+    var details: MutableList<ProjectDetail> = mutableListOf()
+
+    @OneToMany(mappedBy = "project")
+    var skills: MutableList<ProjectSkill> = mutableListOf()
+
+    fun getEndYearMonth(): String {
+        if (endYear == null && endMonth == null) {
+            return "Present"
+        }
+
+        return "${endYear}.${endMonth}"
+    }
+
+    fun update(
+        title: String, description: String,
+        startYear: Int,
+        startMonth: Int,
+        endYear: Int?,
+        endMonth: Int?,
+        isActive: Boolean
+    ) {
+        this.name = title
+        this.description = description
+        this.startYear = startYear
+        this.startMonth = startMonth
+        this.endYear = endYear
+        this.endMonth = endMonth
+        this.isActive = isActive
+    }
+
+    fun addDetail(detail: MutableList<ProjectDetail>?) {
+        if (detail != null) {
+            this.details.addAll(detail)
+        }
+    }
 }
